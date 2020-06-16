aba780d7bd36b8ea526a63993d7cc34229183ff0
jlenotte <jules.lenotte@gmail.com>
2020-06-16 17:33:26 +0200

    [fixed]: added more weight values in the chart

--- app/src/main/java/com/cnam/tuneyourshocks/WeightChartInitializer.java
+++ app/src/main/java/com/cnam/tuneyourshocks/WeightChartInitializer.java
@@ -20,8 +20,11 @@ public class WeightChartInitializer {
         chart.put(80, 175);
         chart.put(90, 195);
         chart.put(100, 215);
+        chart.put(110, 215);
         chart.put(120, 235);
+        chart.put(130, 235);
         chart.put(140, 255);
+        chart.put(150, 255);
         chart.put(160, 275);
 
         return chart;
